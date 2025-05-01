0d0b84ec85fd2cb61473d1ab416903eca2197799
99nirmit <79360867+99nirmit@users.noreply.github.com>
2025-05-01 12:50:22 +0530

    service-registry-eureka

---
+++ StockApp/serviceregistry/src/test/java/com/stockapp/serviceregistry/ServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapp.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
